e2c7835908e90da599ee8ce68cc132e5ac1c495f
Артур Мухайлов <arthurcloud@yandex.ru>
2020-03-02 23:33:38 +0500

    Добавьте файлы проекта.

---
+++ LINQ OBJ 2/Note.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ
+{
+    public class Note
+    {
+        public int _month;          // 1-12
+        public int _year;           // 2000-2010
+        public int _idClient;       // 10-99
+        public int _classDuration;  // 1-30
+
+        public Note(int month, int year, int idClient, int classDuration)
+        {
+            if (month < 1 || month > 12) throw new ArgumentOutOfRangeException("Месяц в диапизоне 1-12!");
+            _month = month;
+
+            if (year < 2000 || year > 2010) throw new ArgumentOutOfRangeException("Год в диапизоне 2000-2010!");
+            _year = year;
+
+            if (idClient < 10 || idClient > 99) throw new ArgumentOutOfRangeException("ID клиента в диапизоне 10-99!");
+            _idClient = idClient;
+
+            if (classDuration < 1 || classDuration > 30) throw new ArgumentOutOfRangeException("Продолжительность занятий в диапизоне 1-30!");
+            _classDuration = classDuration;
+        }
+    }
+}
---
+++ LINQ OBJ 2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ
+{
+    class Program
+    {
+        // Вариант 2 Objects
+        // Задача 2
+        static void Main(string[] args)
+        {
+            List<Note> notes = new List<Note>();
+            try
+            {
+                notes.Add(new Note(1, 2000, 10, 8));
+                notes.Add(new Note(2, 2000, 11, 10));
+                notes.Add(new Note(5, 2001, 12, 14));
+                notes.Add(new Note(8, 2010, 13, 20));
+                notes.Add(new Note(1, 2009, 14, 8));
+                notes.Add(new Note(5, 2009, 15, 20));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return;
+            }
+            var maxNote = notes.Where(note => note._classDuration == notes.Max(x => x._classDuration));
+
+            foreach (var i in maxNote)
+            {
+                Console.WriteLine(string.Format("Максимальная продолжительность = {0},  Год = {1}, Месяц = {2}", i._classDuration, i._year, i._month));
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
+
